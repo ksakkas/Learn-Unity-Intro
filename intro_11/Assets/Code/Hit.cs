5ef3c5d4c2f8338fb1c65b091f910788e1892488
Konstantinos Sakkas <thl1715799@uoi.gr>
2022-01-19 12:42:34 +0200

    intro_11

---
+++ intro_11/Assets/Code/Hit.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit : MonoBehaviour
+{   
+    //OnCollisionEnter καλείται όταν το σώμα του χρήστη έχει αρχίσει να αγγίζει ένα άλλο αντικείμενο - σώμα
+    private void OnCollisionEnter(Collision other) {
+        
+        //GetComponent = Επιστρέφει το στοιχείο (τύπος αντικειμένου) εάν το αντικείμενο του παιχνιδιού είναι συνδεδεμένο και μηδεν εάν δεν είναι.
+        //MeshRenderer = Αποδίδει πλέγματα που έχουν εισαχθεί από το MeshFilter ή το TextMesh .
+
+        GetComponent<MeshRenderer>().material.color = Color.red; 
+    }
+
+    //OnCollisionExit καλείται όταν το σώμα του χρήστη έχει αρχίσει να μην αγγίζει ένα άλλο αντικείμενο - σώμα
+
+    private void OnCollisionExit(Collision other) {
+        GetComponent<MeshRenderer>().material.color = Color.blue; 
+    }
+    
+}
